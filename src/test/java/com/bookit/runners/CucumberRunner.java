a33bcc9e2bfa064f7122f70a079f4842d496a0d6
Tuerhobng Zhumake <ztuerhon@masonlive.gmu.edu>
2020-03-22 13:29:45 -0400

    first commit

---
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -0,0 +1,21 @@
+package com.bookit.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json"
+        },
+
+        features = "src/test/resources/features",
+        glue = "com/bookit/step_definitions",
+        dryRun = true,
+        tags = "@api"
+)
+//and not @ignore means do not run scenarios with this annotation
+public class CucumberRunner {
+}
